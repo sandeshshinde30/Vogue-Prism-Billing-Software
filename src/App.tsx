eddefdf9049355b76c09f685b320b20496c37600
Sandesh Shinde <sandeshshinde7047@gmail.com>
2025-12-18 21:32:28 +0530

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <a href="https://electron-vite.github.io" target="_blank">
+          <img src="logo.png" className="logo" alt="Vite logo" />
+        </a>
+       
+      </div>
+      <h1>Billing Software</h1>
+     
+      
+    </>
+  )
+}
+
+export default App
